fc9aad56e272bc291d44b0039e0591c9a556aecf
Michael Mamuric <799470@ACDM.DS.SAIT.CA>
2020-05-12 18:55:39 -0600

    Initial Commit

---
+++ myscript.lua
@@ -0,0 +1,10 @@
+print("Examples of variables")
+
+-- No variable types defined!
+score = 0
+lives = "Zero"
+
+-- Lua is a dynamically-typed language
+-- Variables do not have types. Only values do!
+print(type(score))
+print(type(lives))
