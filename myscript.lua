65e0cb5f3d8bbced6245701806981421b9ee2bfa
Michael Mamuric <michael.mamuric@edu.sait.ca>
2020-05-12 19:16:45 -0600

    More examples

--- myscript.lua
+++ myscript.lua
@@ -8,3 +8,18 @@ lives = "Zero"
 -- Variables do not have types. Only values do!
 print(type(score))
 print(type(lives))
+
+
+-- Lua Main Value types:
+-- nil -> similar to null
+-- number
+-- string
+-- function
+-- table
+
+salary = 10 / 2
+-- This is how you concatenate in Lua
+print("The value of salary is "..salary)
+
+salary2 = 25 // 2 -- this ensures only the integer part of the value is returned
+print("The value of salary2 is "..salary2)
